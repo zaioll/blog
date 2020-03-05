68e4b8afaab11116ff66fea7eb848a28682631d9
zaioll <zaioll@protonmail.com>
2020-03-05 19:38:56 -0400

    Commit inicial

---
+++ public_html/dia01.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="googlebot" content="">
+
+    <meta name="copyright" content="">  
+    <meta name="revisit-after" content="7 days"> 
+    <meta name=author content="Zaioll">
+
+<!-- Twitter Card -->  
+    <meta name="twitter:card" content="summary_large_image">  
+    <meta name="twitter:site" content="@zaioll">  
+    <meta name="twitter:title" content="Zaioll blog">  
+    <meta name="twitter:description" content="">  
+    <meta name="twitter:image" content="">  
+    <meta name="twitter:url" content="https://twitter.com/zaioll">
+
+    <meta property="og:title" content="Zaioll">  
+    <meta property="og:site_name" content="Zaioll">  
+    <meta property="og:url" content="https://zaioll.com">  
+    <meta property="og:description" content="">  
+    <meta property="og:image" content="">
+    <meta property="og:image" content="">
+
+    <meta property="og:type" content="blog">  
+    <meta property="og:locale" content="pt_BR">  
+    <meta property="og:locale:alternate" content="en_US">  
+    <meta property="og:locale:alternate" content="es_ES">  
+    <meta property="article:author" content="Zaioll">  
+    <meta property="article:publisher" content="">
+
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+   	<link href='http://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet' type='text/css'>
+    <link rel=”alternate” type=”application/rss+xml” href=”http://www.zaioll.com/feed.rss” title=”Crônicas do programador”>
+
+    <title>Zaioll</title>
+
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><img src="feed-icon-14x14.png" width="24" heigth="24" title="" alt=""></li>
+                <li><a href="https://twitter.com/zaioll" target="_blank"><img src="twitter.png" width="24" heigth="24" title="" alt=""></a></li>
+                <li><a href="http://zaioll.com">Home</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <article>
+            <h1>Primeiro dia: O que eu deveria fazer exatamente com esse blog?</h1>
+            <p>
+                Alguns dos grandes super-poderes que eu gostaria de ter são: dormir instantaneamente e aprender mais rápido. A minha profissão exige
+                isso de mim e lógico, de alguma maneira, entrei nessa exatamente por causa destas características. Pensei que seria uma maneira de saber
+                o máximo sobre outras áreas, profissionalmente. Nem tudo são flores. Ainda mais quando se tem filhos, esposa, etc.
+            </p>
+            <p>
+                Antes de chegar no "aprender rápido", despendi muito tempo pra entender o que é aprender e que cada um aprende de uma maneira particular.
+                Pra resumir: Sou cinestésico. Tenho que praticamente trazer o conhecimento seja ele de que tipo for (ai é que a coisa fica complicada), pra realidade
+                material.
+            </p>
+            <p>
+                Pensei em escrever no <a href="http://medium.com/" target="_blank" rel="noopener noreferrer">medium</a>, ou <a href="http://quora.com" target="_blank" rel="noopener noreferrer">Quora</a>, mas qual é a graça ou aprendizado? Pra mim nenhum.
+                Depois de ver um <a href="http://www.youtube.com/watch?v=bU-CWDmzLBM" target="_blank">vídeo</a> do Filipe Deschamps e um <a href="https://willianjusten.com.br/diga-nao-ao-medium-tenha-sua-propria-plataforma/" target="_blank">artigo</a> do o Willian Justen, cheguei a conclusão de que eu não estava tão
+                fora da caixinha assim.
+            </p>
+            <p>
+                Hoje é o primeiro dia desse blog. Veja, apesar de tudo, tem conteúdo no primeiro dia. Eu quis fazer diferente não dos outros, mas
+                da maneira como geralmente eu faço. Muito medo de fazer errado ou "imperfeito". Resolvi colocar em prática o princípio de desenvolvimento
+                progressivo. Isso é tão contra-intuitivo pra mim.
+            </p>
+            <p>
+                A idéia inicial era fazer isso usando <a href="http://www.phoenixframework.org" target="_blank" rel="noopener noreferrer">phoenix</a> e <a href="http://vuejs.org" target="_blank" rel="noopener noreferrer">Vuejs</a>,
+                pois como estou estudando essas duas tecnologias/ferramentas, seria algo útil pra mim. Não deu certo por causa de custos de hospedagem. Eu não tenho 250 bolsonaros para
+                bancar um VPS agora. Sobrou o que tem de monte na internet e que por um acaso é o que me traz o pão: O bom e velho PHP e uma hospedagem gratuíta. E com isso, a idéia acabou mudando também (se você é iniciante, anota esse mantra: "mudança é uma constante")
+                e resolvi fazer o meu próprio blog, do zero, na unha e sem nenhum caralho de framework. Vai ser foda, mas tenho certeza que aprenderei muito mais.  
+            </p>
+            <p>
+                Por isso o site está assim. Não tem nem mesmo uma fonte legal ou pelo menos que facilite a leitura ou pelo menos algum css. Entendo. O foco deste primeiro dia foi colocar o site no ar, ajustar o mínimo
+                pra que ele fique "SEO friendly" e o tio google possa fazer com que ele seja encontrado. Não está o ideal, mas tem alguma coisa. Se inspecionar o código fonte da página,
+                poderá dar uma olhada no que foi feito. Ah, também coloquei a funcionalidade de feed. Como estou viciado nisso. Pra mim é uma ótima maneira de acompanhar e aprender.
+            </p>
+            <p>
+                Os próximos objetivos que tenho agora pra esse blog são:
+                <ol>
+                    <li>Colocar um lugar onde vocês possam interagir comigo</li>
+                    <li>Colocar algo como aquele botão de "palmas" do medim</li>
+                    <li>Colocar um link para algum "me pague um café" rsrs.</li>
+                </ol>
+            </p>
+            <p>
+                No próximo post, além de escrever sobre o que foi feito no próprio blog (motivo da existência disso tudo), vou escrever também sobre
+                as ferramentas que vou usar. Mas vou escrever sobre minha experiência somente. Não tenho como objetivo ser a voz da verdade. Até mais!
+            </p>
+        </article>
+        <aside>
+            <ul>
+               <!--
+                <svg>
+                    <g fill-rule="evenodd">
+                        <path d="M13.74 1l.76 2.97.76-2.97zM16.82 4.78l1.84-2.56-1.43-.47zM10.38 2.22l1.84 2.56-.41-3.03zM22.38 22.62a5.11 5.11 0 0 1-3.16 1.61l.49-.45c2.88-2.89 3.45-5.98 1.69-9.21l-1.1-1.94-.96-2.02c-.31-.67-.23-1.18.25-1.55a.84.84 0 0 1 .66-.16c.34.05.66.28.88.6l2.85 5.02c1.18 1.97 1.38 5.12-1.6 8.1M9.1 22.1l-5.02-5.02a1 1 0 0 1 .7-1.7 1 1 0 0 1 .72.3l2.6 2.6a.44.44 0 0 0 .63-.62L6.1 15.04l-1.75-1.75a1 1 0 1 1 1.41-1.41l4.15 4.15a.44.44 0 0 0 .63 0 .44.44 0 0 0 0-.62L6.4 11.26l-1.18-1.18a1 1 0 0 1 0-1.4 1.02 1.02 0 0 1 1.41 0l1.18 1.16L11.96 14a.44.44 0 0 0 .62 0 .44.44 0 0 0 0-.63L8.43 9.22a.99.99 0 0 1-.3-.7.99.99 0 0 1 .3-.7 1 1 0 0 1 1.41 0l7 6.98a.44.44 0 0 0 .7-.5l-1.35-2.85c-.31-.68-.23-1.19.25-1.56a.85.85 0 0 1 .66-.16c.34.06.66.28.88.6L20.63 15c1.57 2.88 1.07 5.54-1.55 8.16a5.62 5.62 0 0 1-5.06 1.65 9.35 9.35 0 0 1-4.93-2.72zM13 6.98l2.56 2.56c-.5.6-.56 1.41-.15 2.28l.26.56-4.25-4.25a.98.98 0 0 1-.12-.45 1 1 0 0 1 .29-.7 1.02 1.02 0 0 1 1.41 0zm8.89 2.06c-.38-.56-.9-.92-1.49-1.01a1.74 1.74 0 0 0-1.34.33c-.38.29-.61.65-.71 1.06a2.1 2.1 0 0 0-1.1-.56 1.78 1.78 0 0 0-.99.13l-2.64-2.64a1.88 1.88 0 0 0-2.65 0 1.86 1.86 0 0 0-.48.85 1.89 1.89 0 0 0-2.67-.01 1.87 1.87 0 0 0-.5.9c-.76-.75-2-.75-2.7-.04a1.88 1.88 0 0 0 0 2.66c-.3.12-.61.29-.87.55a1.88 1.88 0 0 0 0 2.66l.62.62a1.88 1.88 0 0 0-.9 3.16l5.01 5.02c1.6 1.6 3.52 2.64 5.4 2.96a7.16 7.16 0 0 0 1.18.1c1.03 0 2-.25 2.9-.7A5.9 5.9 0 0 0 23 23.24c3.34-3.34 3.08-6.93 1.74-9.17l-2.87-5.04z"></path>
+                    </g>
+                </svg>
+                -->
+            </ul>
+        </aside>
+    </main>
+</body>
+</html>
